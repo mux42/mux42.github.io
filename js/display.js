9b5fb37e2fb94c61c8576eec1475b2a290285020
maxime <mux99@live.be>
2021-10-11 23:59:38 +0200

    allow icon display in grid

---
+++ js/display.js
@@ -0,0 +1,7 @@
+function display(x,y,file){
+	document.getElementById('board').innerHTML = "<div id=\"G"+x+y+"\"><img src=\""+file+"\"><style type=\"text/css\">#G"+x+y+" {grid-area: G"+x+y+";}</style></div>";
+}
+
+function message(text){
+	//
+}
